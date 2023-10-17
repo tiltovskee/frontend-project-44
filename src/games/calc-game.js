714d7154ade58d9293d381bc13d1fe99807ae51a
Max Moon <poriadoc@yandex.ru>
2023-10-17 20:08:01 +0300

    bug fix

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -34,7 +34,7 @@ const playGame = () => { // инициализация игры
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? '); // запись имени игрока
   console.log(`Hello, ${name}!\nWhat is the result of the expression?`);
-  for (let i = 0; i < 3; i + 1) { // инициализация счетчика раундов
+  for (let i = 0; i < 3; i += 1) { // инициализация счетчика раундов
     const isCorrect = playRound();
     if (!isCorrect) { // проверка результата раунда
       console.log(`Let's try again, ${name}`);
