970fd3286ba2f42684322654b9b089eed5461518
Max Moon <poriadoc@yandex.ru>
2023-10-23 23:56:56 +0300

    update primeGame.js

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
-import getAnswerAndCheck from '../index.js';
+import { getAnswerAndCheck, playGame } from '../index.js';
+import getNumber from '../helpers.js';
 
-const getNumber = () => Math.ceil(Math.random() * 100);
 const isPrime = (num) => {
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0 || num === 1) {
@@ -12,28 +11,13 @@ const isPrime = (num) => {
 };
 
 const playRound = () => {
-  const num = getNumber();
-  let correctAnswer = '';
-  if (isPrime(num)) {
-    correctAnswer = 'yes';
-  } else {
-    correctAnswer = 'no';
-  }
+  const num = getNumber(1, 100);
+  const correctAnswer = (isPrime(num) ? 'yes' : 'no');
   return getAnswerAndCheck(num, correctAnswer);
 };
 
-const playGame = () => { // инициализация игры
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? '); // запись имени игрока
-  console.log(`Hello, ${name}!\nAnswer "yes" if given number is prime. Otherwise answer "no".`);
-  for (let i = 0; i < 3; i += 1) { // инициализация счетчика раундов
-    const isCorrect = playRound();
-    if (!isCorrect) { // проверка результата раунда
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
+const gameQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-export default playGame;
+const playPrimeGame = () => playGame(playRound, gameQuestion);
+
+export default playPrimeGame;
