9970950b06af116b0ae0c12bb36190384b76bc9b
Max Moon <poriadoc@yandex.ru>
2023-10-27 00:31:01 +0300

    update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,23 +1,23 @@
-import { getAnswerAndCheck, playGame } from '../index.js';
-import getNumber from '../helpers.js';
+import playGame from '../index.js';
+import getRandomNumber from '../helpers.js';
 
-const isPrime = (num) => {
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0 || num === 1) {
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0 || number === 1) {
       return false;
     }
   }
   return true;
 };
 
-const playRound = () => {
-  const num = getNumber(1, 100);
-  const correctAnswer = (isPrime(num) ? 'yes' : 'no');
-  return getAnswerAndCheck(num, correctAnswer);
+const getNumberAndAnswer = () => {
+  const number = getRandomNumber(1, 100);
+  const answer = isPrime(number) ? 'yes' : 'no';
+  return [number, answer];
 };
 
 const gameQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const playPrimeGame = () => playGame(playRound, gameQuestion);
+const playPrimeGame = () => playGame(getNumberAndAnswer, gameQuestion);
 
 export default playPrimeGame;
