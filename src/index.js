1b1e76737aeebc67f2d1150c420d9583e47988b1
Max Moon <poriadoc@yandex.ru>
2023-10-27 00:28:58 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import readlineSync from 'readline-sync';
 
-export const getAnswerAndCheck = (guessingSubj, correctAnswer) => {
-  console.log(`Question: ${guessingSubj}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (answer.toLowerCase() === correctAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  return false;
-};
-
-export const playGame = (gameIteration, gameQuestion, numOfRounds = 3) => {
+const playGame = (roundData, gameQuestion, numOfRounds = 3) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!\n${gameQuestion}`);
+  console.log(`Hello, ${playerName}!`);
+  console.log(`${gameQuestion}`);
   for (let i = 0; i < numOfRounds; i += 1) {
-    const isCorrect = gameIteration();
-    if (!isCorrect) {
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
+    const [questionData, correctAnswer] = roundData();
+    console.log(`Question: ${questionData}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer.toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+    } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    return false;
   }
   console.log(`Congratulations, ${playerName}!`);
+  }
 };
+
+export default playGame;
