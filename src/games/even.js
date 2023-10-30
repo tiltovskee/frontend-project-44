6db7fc4a8721d5bc58acdb3a1e600b0cb602d20a
Max Moon <poriadoc@yandex.ru>
2023-10-30 22:33:18 +0300

    update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,16 @@
 import playGame from '../index.js';
 import getRandomNumber from '../helpers.js';
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => (number % 2 === 0 ? true : false);
 
-const getNumberAndAnswer = () => {
+const generateRound = () => {
   const number = getRandomNumber(1, 100);
-  const answer = isEven(number);
+  const answer = isEven(number) ? 'yes' : 'no';
   return [number, answer];
 };
 
 const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const playEvenGame = () => playGame(getNumberAndAnswer, gameQuestion);
+const playEvenGame = () => playGame(generateRound, gameQuestion);
 
 export default playEvenGame;
