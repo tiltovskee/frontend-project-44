55137c22f2db0e8d171f8f565660ec357f4951c8
Max Moon <poriadoc@yandex.ru>
2023-10-30 22:32:28 +0300

    update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,29 +1,29 @@
 import playGame from '../index.js';
 import getRandomNumber from '../helpers.js';
 
-const getProgression = (startNumber, stepOfProgression, lengthOfProgression) => {
+const getProgression = (startNumber, step, length) => {
   const progression = [startNumber];
-  for (let i = 0; i < lengthOfProgression - 1; i += 1) {
-    const arrayElement = progression[i] + stepOfProgression;
+  for (let i = 0; i < length - 1; i += 1) {
+    const arrayElement = progression[i] + step;
     progression.push(arrayElement);
   }
   return progression;
 };
 
-const getProgressionAndAnswer = () => {
+const generateRound = () => {
   const startNumber = getRandomNumber(1, 10);
-  const stepNumber = getRandomNumber(1, 10);
-  const lengthOfProgression = getRandomNumber(5, 15);
-  const progression = getProgression(startNumber, stepNumber, lengthOfProgression);
-  const guessingNumberIndex = getRandomNumber(0, progression.length);
-  const answer = `${progression[guessingNumberIndex]}`;
+  const step = getRandomNumber(1, 10);
+  const length = getRandomNumber(5, 15);
+  const progression = getProgression(startNumber, step, length);
+  const guessingNumberIndex = getRandomNumber(0, progression.length - 1);
+  const answer = String(progression[guessingNumberIndex]);
   progression[guessingNumberIndex] = '..';
-  const progressionForQuestion = progression.join(' ');
-  return [progressionForQuestion, answer];
+  const question = progression.join(' ');
+  return [question, answer];
 };
 
 const gameQuestion = 'What number is missing in the progression?';
 
-const playProgressionGame = () => playGame(getProgressionAndAnswer, gameQuestion);
+const playProgressionGame = () => playGame(generateRound, gameQuestion);
 
 export default playProgressionGame;
