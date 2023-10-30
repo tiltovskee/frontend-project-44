17867177de9a0cba5c35d3cee22c6744f8de8447
Max Moon <poriadoc@yandex.ru>
2023-10-30 22:31:36 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const playGame = (roundData, gameQuestion, numOfRounds = 3) => {
+const playGame = (roundData, question) => {
+  const roundsCount = 3;
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
-  console.log(`${gameQuestion}`);
-  for (let i = 0; i < numOfRounds; i += 1) {
+  console.log(question);
+  for (let i = 0; i < roundsCount; i += 1) {
     const [questionData, correctAnswer] = roundData();
     console.log(`Question: ${questionData}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer.toLowerCase() === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (answer.toLowerCase() !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${playerName}!`);
-      return false;
+      return;
     }
-    console.log(`Congratulations, ${playerName}!`);
+    console.log('Correct!');
   }
-  return null;
+  console.log(`Congratulations, ${playerName}!`);
 };
 
 export default playGame;
